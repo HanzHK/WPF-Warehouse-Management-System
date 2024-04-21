71234ed7da707c29619b812bce282a360f54bf1f
HanzHKR <kubicek.jan@hotmail.com>
2024-04-21 02:20:39 +0200

    basic integrace MahApps.Metro frameworku pro lepší vzhled aplikace

--- sklad_hustota_zasilky/MainWindow.xaml.cs
+++ sklad_hustota_zasilky/MainWindow.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
+using MahApps.Metro.Controls;
 
 namespace sklad_hustota_zasilky
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
